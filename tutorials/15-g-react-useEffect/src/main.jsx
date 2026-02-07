87c48980ed7a34e38f376ffd39b9d3b93db71dc2
YourName <you@example.com>
2026-02-07 16:35:21 +0000

    renamed folder and added useEffect

---
+++ tutorials/15-g-react-useEffect/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "../index.css";
+
+const rootDivElement = document.getElementById("root");
+
+const reactRoot = ReactDOM.createRoot(rootDivElement);
+
+reactRoot.render(<App />);
