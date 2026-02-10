5f578d1a12f230d98e6421428afeede658a1a917
YourName <you@example.com>
2026-02-10 08:08:25 +0000

    renamed folders, added a typescript conversion tutorial folder

---
+++ tutorials/22-typescript/src/App.tsx
@@ -0,0 +1,19 @@
+import CenterButton from "./components/CenterButton";
+import ImageLoader from "./components/ImageLoader";
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#1e1e2f] text-neutral-100 font-sans">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <h1 className="mt-4 text-3xl font-bold">Good Morning</h1>
+        <h2 className="mt-4 text-2xl font-bold">Time for Coffee</h2>
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+          tempore.
+        </p>
+        <CenterButton />
+        <CenterButton />
+        <ImageLoader />
+      </main>
+    </div>
+  );
+}
