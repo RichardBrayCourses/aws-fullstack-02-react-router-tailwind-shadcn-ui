3645d5da0b1f76b1708229f85ab1e775039f7f7f
Your Name <you@example.com>
2026-02-18 09:55:37 +0000

    directory rename after react-tailwind

--- tutorials/17-react-tailwind/src/App.jsx
+++ tutorials/17-react-tailwind/src/App.jsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <p className="bg-sky-500">"Hello Mister Blue Sky"</p>;
+  return (
+    <div className="min-h-screen flex flex-col bg-[#1e1e2f] text-neutral-100 font-sans">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <h1 className="mt-4 text-3xl font-bold">Good Morning</h1>
+        <h2 className="mt-4 text-2xl font-bold">Time for Coffee</h2>
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+          tempore.
+        </p>
+        <div id="button-location"></div>
+      </main>
+    </div>
+  );
 };
 
 export default App;
