e3a96167d96cd18eca629ec90901e006c52d7bf8
YourName <you@example.com>
2026-02-19 16:40:26 +0000

    event listener bugfix

--- tutorials/21-react-side-effects/src/components/CenterButton.jsx
+++ tutorials/21-react-side-effects/src/components/CenterButton.jsx
@@ -14,6 +14,7 @@ const CenterButton = () => {
       console.log("activated, listener added");
     } else {
       setActive(false);
+      window.removeEventListener("keydown", logKey);
       console.log("deactivated, listener removed");
     }
   };
