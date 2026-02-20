8b1477fd33a2194c809334f36b88cbced9834a41
Your Name <you@example.com>
2026-02-20 07:34:09 +0000

    unwanted side effects

--- tutorials/22-react-side-effect-unwanted-behaviour/src/App.jsx
+++ tutorials/22-react-side-effect-unwanted-behaviour/src/App.jsx
@@ -6,15 +6,15 @@ const App = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-[#1e1e2f] text-neutral-100 font-sans">
-      <main className="flex-1 flex flex-col items-center justify-center">
+      <main className="flex-1 flex flex-col items-center justify-top">
         <h1 className="mt-4 text-3xl font-bold">Good Morning</h1>
         <h2 className="mt-4 text-2xl font-bold">Time for Coffee</h2>
 
         <button
-          className="mt-4 bg-white text-black py-1 px-3 rounded"
+          className="mt-4 bg-sky-500 text-black py-1 px-3 rounded"
           onClick={() => setShowButton((prev) => !prev)}
         >
-          Toggle CenterButton
+          Toggle Logging Button
         </button>
 
         {showButton && <CenterButton />}
