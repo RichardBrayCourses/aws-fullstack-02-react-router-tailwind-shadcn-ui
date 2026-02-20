3647aca19a2f9551326285490b7733d9a256779c
Your Name <you@example.com>
2026-02-20 09:00:19 +0000

    react side effects unwanted behaviour

--- tutorials/22-react-side-effect-unwanted-behaviour/src/App.jsx
+++ tutorials/22-react-side-effect-unwanted-behaviour/src/App.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import CenterButton from "./components/CenterButton.jsx";
-
 const App = () => {
-  const [showButton, setShowButton] = useState(true);
+  const [show, setShow] = useState(true);
 
   return (
     <div className="min-h-screen flex flex-col bg-[#1e1e2f] text-neutral-100 font-sans">
       <main className="flex-1 flex flex-col items-center justify-top">
         <h1 className="mt-4 text-3xl font-bold">Good Morning</h1>
         <h2 className="mt-4 text-2xl font-bold">Time for Coffee</h2>
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+          tempore.
+        </p>
 
         <button
           className="mt-4 bg-sky-500 text-black py-1 px-3 rounded"
-          onClick={() => setShowButton((prev) => !prev)}
+          onClick={() => setShow((prev) => !prev)}
         >
           Toggle Logging Button
         </button>
 
-        {showButton && <CenterButton />}
+        {show && <CenterButton />}
       </main>
     </div>
   );
