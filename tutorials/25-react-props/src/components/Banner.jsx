e16af92e9ea0b2cbc3f672c9e6648f1666999e89
YourName <you@example.com>
2026-02-22 09:10:47 +0000

    banner and click button initial values

--- tutorials/25-react-props/src/components/Banner.jsx
+++ tutorials/25-react-props/src/components/Banner.jsx
@@ -13,7 +13,7 @@ export default Banner;
 const Banner = () => {
   return (
     <div className="w-32 h-32 bg-slate-800 rounded flex items-center justify-center mt-4 mx-auto text-6xl font-bold">
-      content
+      56
     </div>
   );
 };
