553283827bf1739ad136fd56f9f894dc71673aff
Your Name <you@example.com>
2026-02-23 08:26:55 +0000

    banner simplified

--- tutorials/28-react-context/src/components/Banner.jsx
+++ tutorials/28-react-context/src/components/Banner.jsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { CounterContext } from "../context/CounterContext.jsx";
 
 const Banner = () => {
-  const { counter } = useContext(CounterContext);
+  const value = useContext(CounterContext);
+  const { counter } = value;
 
   return (
     <div className="w-32 h-32 bg-slate-800 rounded flex items-center justify-center mt-4 mx-auto text-6xl font-bold">
