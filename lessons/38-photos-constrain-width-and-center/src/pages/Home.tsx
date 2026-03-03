8e197be08b75a605624b8e815590d81734d63abd
Your Name <you@example.com>
2026-03-03 10:16:10 +0000

    image styling lesson tidyup

--- lessons/38-photos-constrain-width-and-center/src/pages/Home.tsx
+++ lessons/38-photos-constrain-width-and-center/src/pages/Home.tsx
@@ -1,13 +1,19 @@
-import photos from "@/data/photos";
+import { PhotoData, photos } from "@/data/photos";
 
-export default function Home() {
+function transformer(photo: PhotoData, index: number) {
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      {photos.map((photo) => (
-        <div key={photo.id} className="mb-8">
-          <img src={photo.small} alt={photo.title} className="w-full h-auto" />
-        </div>
-      ))}
+    <div key={index} className="mb-8">
+      <img
+        src={index % 2 == 0 ? photo.small : photo.large}
+        alt={photo.title}
+        className="w-full h-auto"
+      />
     </div>
   );
 }
+const Home = () => {
+  const array = photos.map(transformer);
+  return <div className="max-w-3xl mx-auto p-4">{array}</div>;
+};
+
+export default Home;
