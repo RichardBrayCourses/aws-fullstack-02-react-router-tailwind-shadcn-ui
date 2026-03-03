accbcd897c9c78c75444faeb8de191c23c48929c
YourName <you@example.com>
2026-03-03 17:38:05 +0000

    small change to  captions

--- lessons/40-photos-caption-overlay/src/pages/Home.tsx
+++ lessons/40-photos-caption-overlay/src/pages/Home.tsx
@@ -9,18 +9,15 @@ function transformer(photo: PhotoData, index: number) {
       <img
         src={index % 2 === 0 ? photo.small : photo.large}
         alt={photo.title}
-        className="w-full h-auto transition-transform duration-500 group-hover:scale-105"
-        loading="lazy"
+        className="w-full h-auto transition-transform duration-700 group-hover:scale-105"
       />
-
-      <div className="absolute inset-x-0 bottom-0 p-4 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity">
+      <div className="absolute inset-x-0 bottom-0 p-4 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 duration-700 ">
         <div className="text-white text-sm font-semibold">{photo.title}</div>
         <div className="text-white/80 text-xs">{photo.description}</div>
       </div>
     </div>
   );
 }
-
 const Home = () => {
   const array = photos.map(transformer);
   return (
