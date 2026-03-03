589bb1949e90f2d37e0b82840aeabfc8fb64e765
YourName <you@example.com>
2026-03-03 05:40:47 +0000

    created displayPhoto function in lesson 37

--- lessons/37-photos-basic-render-no-layout/src/pages/Home.tsx
+++ lessons/37-photos-basic-render-no-layout/src/pages/Home.tsx
@@ -1,13 +1,52 @@
-import photos from "@/data/photos";
+/* 
 
-export default function Home() {
-  return (
-    <div>
-      {photos.map((photo) => (
-        <div key={photo.id}>
-          <img src={photo.small} alt={photo.title} />
-        </div>
-      ))}
-    </div>
-  );
-}
+const displayPhoto = (photo: PhotoData) => photo.title;
+
+const displayPhoto = (photo: PhotoData) => <div>photo.title</div>;
+
+const displayPhoto = (photo: PhotoData) => <div>{photo.title}</div>;
+
+const displayPhoto = (photo: PhotoData) => (
+  <div key={photo.id}>{photo.title}</div>
+);
+
+const displayPhoto = (photo: PhotoData) => (
+  <div key={photo.id}>
+    <img src={photo.small} />
+  </div>
+);
+
+
+
+const displayPhoto = (photo: PhotoData) => (
+  <div key={photo.id}>
+    <img src={photo.small} />
+  </div>
+);
+
+const displayPhoto = (photo: PhotoData) => (
+  <div key={photo.id}>
+    <img src={photo.small} alt={photo.title} />
+  </div>
+);
+
+
+const Home = () => {
+  return <div className="flex-1 ">{photos.map(displayPhoto)}</div>;
+};
+
+*/
+
+import { photos, PhotoData } from "@/data/photos";
+
+const displayPhoto = (photo: PhotoData) => (
+  <div key={photo.id}>
+    <img src={photo.small} alt={photo.title} />
+  </div>
+);
+
+const Home = () => {
+  return <div className="flex-1 ">{photos.map(displayPhoto)}</div>;
+};
+
+export default Home;
