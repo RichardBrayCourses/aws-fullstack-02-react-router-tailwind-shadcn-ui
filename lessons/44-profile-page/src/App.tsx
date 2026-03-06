a34f3aeccbe7d4241fa9669f9e097ba5908fd1d6
YourName <you@example.com>
2026-03-06 16:20:59 +0000

    header user margin change, added lesson 44 profile page

--- lessons/44-profile-page/src/App.tsx
+++ lessons/44-profile-page/src/App.tsx
@@ -11,7 +11,7 @@ const AppContent = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 ">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
